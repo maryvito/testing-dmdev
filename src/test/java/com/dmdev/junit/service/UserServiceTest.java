7bce49d15bed0f5bb28c6b8b0e77cca8bf2483d1
maryvito <maryv2@yandex.ru>
2024-06-12 18:56:08 +0300

    .

---
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.dmdev.junit.service;
+
+import com.dmdev.junit.dto.User;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserServiceTest {
+
+    UserService userService;
+
+    @BeforeAll
+    void before(){
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare(){
+        System.out.println("before each: " + this);
+        userService = new UserService();
+    }
+
+    @Test
+    void usersEmptyIfNoUserAdded(){
+        System.out.println("Test 1: " + this);
+
+        var users = userService.getAll();
+        assertTrue(users.isEmpty(), () -> "User list should be empty");
+    }
+
+    @Test
+    void usersSizeIfUserAdded(){
+        System.out.println("Test 2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase(){
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool(){
+        System.out.println("Before all: " + this);
+    }
+}
