9919bd7bc11b80847715d78dcad6ef6676157672
maryvito <maryv2@yandex.ru>
2024-06-13 15:30:12 +0300

    .

--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -7,10 +7,13 @@ import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -137,7 +140,35 @@ public class UserServiceTest {
             assertTrue(maybeUser.isEmpty());
         }
 
+        @ParameterizedTest(name = "{arguments} test")
+        //@ArgumentsSource()
+        @NullSource
+        @EmptySource
+        @ValueSource(strings = {
+                "Ivan", "Petr"
+        })
+     //   @EnumSource
+       // @MethodSource("com.dmdev.junit.service.UserServiceTest#getArgumentsForLoginTest")
+       // @CsvFileSource(resources = "/login-test-data.csv", delimiter = ',', numLinesToSkip = 1)
+        @CsvSource({
+                "Ivan,123",
+                "Petr,111"
+        })
+        void loginParameterizedTests(String username, String password){
+            userService.add(IVAN, PETR);
 
+            var maybeUser = userService.login(username, password);
+           // assertThat(maybeUser).isEqualTo(user);
+        }
 
+    }
+
+    static Stream<Arguments> getArgumentsForLoginTest(){
+        return Stream.of(
+                Arguments.of("Ivan", "123", Optional.of(IVAN)),
+                Arguments.of("Petr", "111", Optional.of(PETR)),
+                Arguments.of("Petr", "dummy", Optional.empty()),
+                Arguments.of( "dummy", "123", Optional.empty())
+        );
     }
 }
